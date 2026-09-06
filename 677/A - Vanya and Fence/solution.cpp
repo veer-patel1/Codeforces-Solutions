0542b76330e6a18ced763f43c043648e7fe3384e
veer-patel1 <veermayurpatel1121@gmail.com>
2026-09-06 22:12:49 +0530

    Add Vanya and Fence [A] from Codeforces

---
+++ 677/A - Vanya and Fence/solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int n, h;
+    cin >> n >> h;
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        if (x > h)
+            ans += 2;
+        else
+            ans += 1;
+    }
+    cout << ans << endl;
+    return 0;
+}
