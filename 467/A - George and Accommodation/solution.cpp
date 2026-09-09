ea6367188f06d28c2285c7ce26f2fb3832516cca
veer-patel1 <veermayurpatel1121@gmail.com>
2026-09-09 23:51:40 +0530

    Add George and Accommodation [A] from Codeforces

---
+++ 467/A - George and Accommodation/solution.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+    int ans = 0;
+    while (n--) {
+        int p, q;
+        cin >> p >> q;
+        if (q - p >= 2)
+            ans++;
+    }
+    cout << ans << '
+';
+    return 0;
+}
